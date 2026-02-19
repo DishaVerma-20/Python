f5c269d2a3972e69e518404240d59c777057bac0
DishaVerma <ms.dishaverma.20@gmail.com>
2026-02-19 11:08:45 +0530

    1st commit in this folder

---
+++ Chapter 1/module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+# print("printing jokes") ctrl + /
+joke = pyjokes.get_joke()
+print(joke)
+
+# multiline comments: triple single/ double quotes se bna skte haii
+# but ye actual mai multiline strings hota hai real comment nahi
+# single line comment he actual real comment hota haii
+
