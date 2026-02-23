34cd8690728ae18c4a868a0c4dde1ad6f80efd9c
DishaVerma <ms.dishaverma.20@gmail.com>
2026-02-23 21:58:02 +0530

    2nd commit in this folder

---
+++ Chapter 2/04_operators.py
@@ -0,0 +1,22 @@
+# Arithmetic Operator
+
+a = 78
+b = 65
+c = a+b
+print(c)
+
+# Assignment Operators
+
+a = 7
+a += 6 # Increment the value of a by 6
+print(a)
+
+a -= 2 # Decrement the value of a by 2
+
+# Comparison Operator hamesha Boolean Value return karte haii
+a = 7
+b = int(input())
+r = a == b
+s = a!=b
+print (r, s)
+
