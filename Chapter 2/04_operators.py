9a8306fb89dffbf7b1cc833f273246e5b8812db6
DishaVerma <ms.dishaverma.20@gmail.com>
2026-02-25 22:04:39 +0530

    Feb 25 2026

--- Chapter 2/04_operators.py
+++ Chapter 2/04_operators.py
@@ -20,3 +20,20 @@ r = a == b
 s = a!=b
 print (r, s)
 
+
+# Logical Operators
+
+# Truth table of 'or'
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth Table of 'and'
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
+
+# Not operator
+print(not(False))
---
+++ Chapter 2/05_type.py
@@ -0,0 +1,11 @@
+a = 90
+t = type(a)
+print(t)
+
+b = 67.54
+c = "56"
+d = "Disha"
+# print(type(b, c, d))
+print(type(b))
+print(type(c))
+print(type(d))
