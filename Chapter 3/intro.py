67feaee63b04c2d5d5921e476c4a41a6657be1c9
DishaVerma <ms.dishaverma.20@gmail.com>
2026-02-27 10:30:18 +0530

    String slicing

---
+++ Chapter 3/intro.py
@@ -0,0 +1,14 @@
+name = "Disha Verma"
+name1 = 'disha'
+name2 = '''hello''' # MULTI STRING
+
+
+# string slicing
+print(name[:8]) # start from 0
+print(name[:])
+print(name[0:]) # length of the string
+print(name[-3:-11])
+
+# For converting : positive_index = len(string) + negative_index
+# left to right hoti hai indexing
+
