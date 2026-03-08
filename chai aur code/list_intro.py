4c43ddba3ad9f59ec90c8aab0de8c4a8c77b8030
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-08 23:10:11 +0530

    list completed

--- chai aur code/list_intro.py
+++ chai aur code/list_intro.py
@@ -26,3 +26,17 @@ tea_varities.remove("test")
 # to remove the particular element from the list
 print(tea_varities)
 
+# inserting elemnt at particular position
+tea_varities.insert(1, "Turmeric")
+# to copy the list with different memory reference
+tea_varities_copy = tea_varities.copy()
+print("this is copy", tea_varities_copy)
+
+# List Comprehension in Python is a short and elegant way to create a list using a single line of code instead of writing a full for loop.
+# [expression for item in iterable]
+
+square_nums = [x**2 for x in range(10)]
+print(square_nums)
+
+cube_nums = [x**3 for x in range(7)]
+print(cube_nums)
