c7afe23783748cae0c0cff39227366b3a6d7f4a6
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-09 21:43:48 +0530

    About to complete

--- chai aur code/dict_intro.py
+++ chai aur code/dict_intro.py
@@ -23,3 +23,32 @@ for a in d:
 # key value dono print krne ke liye
 for a in d:
     print("Key: value = ",a, d[a])
+
+
+# key + value = item
+for key, value in d.items():
+    print(key, value)
+
+# if we want to check particular key exist kar rahi hai in dictionary or not
+# conditional
+if "name" in d:
+    print("My name is Disha Verma.")
+
+# to find the length of a dictionary
+print(len(d))
+
+chai_types = {'Masala': 'Spicy', 'Ginger': 'Zesty', 'Mint': 'Mild', 'Green': 'Mild'}
+
+# to add a n item in a dict that is key-value pair
+chai_types["Earl Grey"] = "Citrus"
+print("New list is ", chai_types)
+
+# to remove an element from the dictionary using pop
+chai_types.pop("Mint")
+print(chai_types)
+
+# to remove the last entered element
+print(chai_types.popitem())
+
+# for deleting the element from the memory reference
+del chai_types["Green"]
