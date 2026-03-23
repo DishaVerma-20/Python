21b16ad250ff610877677a313446b17e4f185984
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-23 09:41:19 +0530

    q2 done

---
+++ chai aur code/Functions/q2.py
@@ -0,0 +1,9 @@
+# Function with Multiple Parameters
+
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+
+def sums(a, b):
+    return a+b
+print(sums(6,7))
+
+# “Without print, function works silently. Without return, function cannot give output for reuse.”
