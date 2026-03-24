e943f7710e047c28d0d7cc9da3a427b1e65ccb9f
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-24 11:20:05 +0530

    ques 5 done!

--- chai aur code/Functions/q5.py
+++ chai aur code/Functions/q5.py
@@ -3,7 +3,11 @@
 # Problem: Write a function that greets a user. If no name is provided, it should greet with a default name.
 
 def greet(name = "Disha"):
-    print("Good morning", name)
+    # print("Good morning", name) 
+    # we can also use return
+    return "Hello" + name + "!"
 greet()
 greet("Verma")
 # greet(Verma) Verma is not defined
+
+# jo bhi parameter hota hai vo required ho jaat haiii
