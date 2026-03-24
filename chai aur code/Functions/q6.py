04b187ed0107464860e886f424f7db06fe41df4b
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-24 11:24:05 +0530

    ques 6 done

--- chai aur code/Functions/q6.py
+++ chai aur code/Functions/q6.py
@@ -2,9 +2,10 @@
 
 # Problem: Create a lambda function to compute the cube of a number.
 
-def cube(a):
+def cubes(a): # parameters, placeholders
     return a * a * a
-print(cube(int(input("Enter number: "))))
+    # return a ** 3 , uppr vale return ka pdega srf uske baad nahii
+print(cubes(int(input("Enter number: "))))
 
-
-
+cube = lambda x: x*x*x # short functions, frameworks mai use hota hai, flask, on the GO chije
+print(cube(9)) # arguments
