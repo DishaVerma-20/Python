a3f103e5358643980ecc7ddb2f8e7e4776e1245f
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-28 12:52:54 +0530

    scopes

---
+++ chai aur code/scopes/basic.py
@@ -0,0 +1,11 @@
+# Matlab Python ke paas ek list/dictionary hoti hai jisme: name → value stored hota hai
+username = "disha"
+username = "chai"
+def func():
+    # username = "chai"
+    # hmne yaha comment kr diya isko, mtlb function mai usename nahi hai ab yeh uppr jayga aur username search karega, jo mil jayga usey le lega, jo badme aayga vo
+    print(username)
+
+print(username)
+func()
+
