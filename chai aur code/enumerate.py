47f203eb644b54baae6668d725d8f987d0fd36cd
DishaVerma <ms.dishaverma.20@gmail.com>
2026-04-06 21:05:34 +0530

    enumeration

--- chai aur code/enumerate.py
+++ chai aur code/enumerate.py
@@ -5,3 +5,9 @@ print(list(y))
 
 # gives index + value
 # more pythonic
+
+
+list = [{'name':'Disha', 'time':'2 min'}, {'name':'Suhani', 'time':'3 min'}]
+a = enumerate(list, start = 1)
+for i, vid in a:
+    print(f"{i}, {vid['name']}") # video mai bhi 2 elements haii
