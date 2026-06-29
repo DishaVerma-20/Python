3617f66d93ed16e889ff97fb12e3b471098c4466
DishaVerma <ms.dishaverma.20@gmail.com>
2026-06-29 16:09:58 +0530

    stack done!

---
+++ DSA with Python/stack.py
@@ -0,0 +1,30 @@
+class Stack:
+    def __init__(self):
+        self.s = []
+    
+    def length(self):
+        return len(self.s)
+    
+    def push(self, value):
+        self.s.insert(0, value) # hmesha tos 0 he hoga
+
+    def peek(self):
+        if (len(self.s)==0):
+            raise Exception("Stack is empty")
+        else:
+            return self.s[0]
+        
+    def pop(self):
+        if len(self.s)==0:
+            raise Exception("Stack is empty")
+        else:
+            return self.s.pop(0)
+    
+stk = Stack()
+stk.push(10)
+stk.push(20)
+stk.push(30)
+stk.push(40)
+print(stk.peek())
+print(stk.pop())
+print(stk.pop())
