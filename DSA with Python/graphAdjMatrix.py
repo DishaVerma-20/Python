4a4cc89bdaaa20efcac4cefb758f21f7ac552132
DishaVerma <ms.dishaverma.20@gmail.com>
2026-07-04 17:35:26 +0530

    Graph using adjacency matrix

---
+++ DSA with Python/graphAdjMatrix.py
@@ -0,0 +1,34 @@
+class Graph:
+    def __init__(self, vertex):
+        self.mat = [[0]*vertex for x in range(vertex)]
+        self.size = vertex
+
+    def add_edge(self, src, dest):
+        if (0 <= src < self.size and 0 <= dest < self.size):
+
+            # undirected graph
+            self.mat[src][dest] = 1
+            self.mat[dest][src] = 1
+
+            # directed graph
+            # self.mat[src][dest] = 1
+
+    def add_edge_weighted(self, src, dest, weight):
+        if (0<=src<self.size and 0<=dest<self.size):
+            self.mat[src][dest] = weight
+            self.mat[dest][src] = weight
+        else:
+            print("Invalid edge")
+        
+    def print(self):
+        for row in self.mat:
+            print(' '.join(map(str, row)))
+
+G = Graph(5)
+G.add_edge(0, 1)
+G.add_edge(0, 2)
+G.add_edge(1, 3)
+G.add_edge(2, 3)
+G.add_edge(2, 4)
+G.add_edge(3, 4)
+G.print()
