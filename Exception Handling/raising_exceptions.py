2f1d7e3f94b372ea4d4e0bcc4aa4b3957bfdce1d
DishaVerma <ms.dishaverma.20@gmail.com>
2026-07-30 13:05:14 +0530

    raising exception done

---
+++ Exception Handling/raising_exceptions.py
@@ -0,0 +1,16 @@
+def check_age(age:int):
+    if age < 0:
+        raise ValueError("Age cannot be negative")
+    elif (age >= 150):
+        raise ValueError("Age is not realistic")
+    print("Your age is good")
+    
+# check_age(1000)
+
+try:
+    check_age(abc)
+except ValueError as e:
+    print(e)
+except Exception as e:
+    print(type(e).__name__)
+    print(e)
