c8d93e289a7797711a25a18c1b5dd01dce2c2c74
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-01 00:00:03 +0530

    read with with done

--- File Handling/read2.py
+++ File Handling/read2.py
@@ -1,7 +1,19 @@
-f = open("hello.txt")
-content = f.read(10)
-print(content)
-content2 = f.read(10) # next 10 char
-print(content2)
-content3 = f.read(10)
-print(content3)
+# f = open("hello.txt")
+# content = f.read(10)
+# print(content)
+# content2 = f.read(10) # next 10 char
+# print(content2)
+# content3 = f.read(10)
+# print(content3)
+
+# f.close()
+# content = f.read(10) 
+# error aa jayga badme
+
+# but isme error aayga toh file close nahi ho paygi kyuki program ka execution toh vhii rukk gaya nh
+with open("hello.txt") as f:
+    content = f.read(10)
+    print(content)
+    content = f.read(10)
+    print(content)
+    # new line bhi 1 char he hai
