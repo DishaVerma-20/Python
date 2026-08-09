e43a985dc79ff03780eeeccd87aa54dc358e5a40
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-09 17:02:51 +0530

    Linked List 1st ques done

--- DSA Questions/masterji.co/delete_kth_Node.py
+++ DSA Questions/masterji.co/delete_kth_Node.py
@@ -38,46 +38,6 @@ class Solution:
             count += 1
         return head
 
-"""
-Given a singly linked list, remove every **kth** node from the list (using 1 - based indexing). It is guaranteed that **k** is less than or equal to the length of the list. After removal, the remaining nodes should stay in their original order.
-
-Input:  List: 1 → 2 → 3 → 4 → 5 → 6,  k = 2
-Output: 1 → 3 → 5
-Explanation: Every 2nd node (2, 4, 6) is removed, leaving 1, 3, and 5.
-"""
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-class Solution:
-    def removeEveryKthNode(self, head: ListNode | None, k: int) -> ListNode | None:
-        if head is None:
-            return None
-            
-        curr = head
-        prev = None
-        count = 1
-
-        while curr:
-            if count % k == 0:
-                
-                if prev is None:
-                    head = curr.next
-                    curr = head
-                else:
-                    prev.next = curr.next
-                    curr = curr.next
-
-            else:
-                prev = curr
-                curr = curr.next
-
-            count += 1
-        return head
-
 head = ListNode(1)
 head.next = ListNode(2)
 head.next.next = ListNode(3)
