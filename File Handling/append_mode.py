02fe7fa7f3ff15ae5a221f747b8c1f6118fb9e59
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-12 14:32:24 +0530

    append mode done

---
+++ File Handling/append_mode.py
@@ -0,0 +1,7 @@
+with open("new.txt", "a") as f:
+    f.write("Hello World\n")
+    f.write("absckefkn\n")
+    f.write("Hello world\n")
+
+
+# yha cheeje at the end aakr edit ho jaati hai, without rewriting the existing ones..
