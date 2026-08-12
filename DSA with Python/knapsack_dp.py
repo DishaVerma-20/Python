eda170bb5cbade8bf876818b917c00cfd88b78b0
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-12 13:32:32 +0530

    knapsack using dynamic programming done

--- DSA with Python/knapsack_dp.py
+++ DSA with Python/knapsack_dp.py
@@ -6,4 +6,13 @@ def knapsackDP(wt, value, capacity):
     for i in range(1, n+1):
         for w in range(capacity + 1):
             if (wt[i-1]<=w):
-                dp[i][w] = max(dp[i-1][w], dp[])
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w-wt[i-1]]+value[i-1])
+            else:
+                dp[i][w] = dp[i-1][w]
+    print("Max profit", dp[n][capacity])
+
+weights = [2, 3, 4, 5]
+values = [3, 4, 5, 6]
+capacity = 5
+
+knapsackDP(weights, values, capacity)
