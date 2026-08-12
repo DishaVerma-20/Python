ce9414ba3c3e889f0cd952c32ca5d837c5e0d9db
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-12 14:36:28 +0530

    exclusive mode done

---
+++ File Handling/x_exclusiveMode.py
@@ -0,0 +1,8 @@
+# agar hmari file exists already, so it will throw an error
+# with open("new.txt", "x") as f:
+#     f.write("abcadv")
+# FlieExistsError
+
+with open("neww.txt", "x") as f:
+    f.write("abcadvnjh")
+    # it will create a new file named neww.txt
