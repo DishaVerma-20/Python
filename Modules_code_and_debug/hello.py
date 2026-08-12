7bb59b028b75ca31ab78859b0d572c2ead13fad5
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-12 15:51:29 +0530

    basic of module smjh aa gaya

---
+++ Modules_code_and_debug/hello.py
@@ -0,0 +1,12 @@
+import math as m
+# this is the best way to use
+# Namespace is a container that maps names to objects and prevents naming conflicts.
+
+print(m.sqrt(16))
+print(m.ceil(7.23))
+print(m.floor(7.23))
+# mtlb math yha pr act krega as an namespace
+
+# from math import *
+# iss folder ki saari files bahar current file mai le aao 
+print(m.pi)
