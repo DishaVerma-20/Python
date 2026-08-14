680c26a793833b307c5f03320580527a46e12527
DishaVerma <ms.dishaverma.20@gmail.com>
2026-08-14 10:53:57 +0530

    reverse linked list done

---
+++ LinkedList/reverse_flip.py
@@ -0,0 +1,31 @@
+class ListNode:
+    def __init__(self, value = 0, next = None):
+        self.value = value
+        self.next = next
+
+class Solution:
+    def reverse_linked_list(self, head):
+        temp = head
+        prev = None
+
+        while temp != None:
+            t1 = temp.next
+            temp.next = prev
+            prev = temp
+            temp = t1
+        return prev
+
+head = ListNode(1)
+head.next = ListNode(3)
+head.next.next = ListNode(2)
+head.next.next.next = ListNode(5)
+
+result = Solution().reverse_linked_list(head)
+
+temp = result
+while temp != None:
+    if temp.next == None:
+        print(temp.value, end = "")
+    else:
+        print(temp.value, end=" -> ")
+    temp = temp.next
